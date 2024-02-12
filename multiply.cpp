12bb393a62e1700f57c96cea4f1d8e8c177b59e2
nitin <2020pgicsnitin53@poornima.org>
2024-02-12 23:21:07 +0530

    Added multiply file

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	cout << "Enter two numbers: " << endl;
+	cin >> a >> b;
+	
+	cout << "The product of the two numbers is: " << a * b << endl;
+
+	return 0;
+}
